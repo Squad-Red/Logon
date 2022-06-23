b26401c3863cd1ef3d3e9546a3374c1d0cc64b09
Jean-Stefan <jeanss12@gmail.com>
2022-06-23 17:04:32 -0300

    Style: Added Weather style

---
+++ src/components/Weather/Weather.styled.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Location = styled.p`
+    font-size: 0.875rem;
+    color: #222222;
+`;
+
+export const WeatherGroup = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const Temperature = styled.span`
+    font-size: 3rem;
+    font-weight: 700;
+    color: #222222;
+    line-height: 0.625rem;
+`;
+
+export const WeatherIcon = styled.img`
+    width: 2.125rem;
+    height: 2.125rem;
+    margin-bottom: 0.3125rem;
+`;
