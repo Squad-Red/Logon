95ad3ce33e4178551c5fcc9df73f02401ec7a986
Brunnalage <brunnalage@gmail.com>
2022-06-23 20:54:49 -0300

    Refactor: Changing measurements in px to rem using the 'toRem' conversion function

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { toRem } from "../../utils/convertToRem";
 
 const StyledButton = styled.button`
 
@@ -8,33 +9,33 @@ const StyledButton = styled.button`
     font-weight: 700;
 
     &.Login {
-    width: 379px;
-    height: 67px;    
+    width: ${toRem(379)};
+    height: ${toRem(67)};    
     background: linear-gradient(90deg, #FF2D04 0%, #C13216 100%) padding-box, 
                 linear-gradient(90deg, #FF2D04 0%, #C13216 100%) border-box;
-    box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.15), 5px 5px 15px rgba(0, 0, 0, 0.5);   
-    border: 2px transparent;
-    border-radius: 50px;  
+    box-shadow: inset ${toRem(5)} ${toRem(5)} ${toRem(15)} rgba(0, 0, 0, 0.15), ${toRem(5)} ${toRem(5)} ${toRem(15)} rgba(0, 0, 0, 0.5);   
+    border: 0.125rem transparent;
+    border-radius: ${toRem(50)};  
     }
 
     &.Logout {
-    width: 89px;
-    height: 55px;
+    width: ${toRem(89)};
+    height: ${toRem(55)};
     background: transparent;
     border: none;
     }
 
     &.ResetTimer {
-    width: 131px;
-    height: 99px;
+    width: ${toRem(131)};
+    height: ${toRem(99)};
     background: #FFFFFF;
     color: #C13216;
     font-weight: 400;
     font-size: 0.75rem; 
     border: none;
     @media (max-width: 768px) {
-    width: 350px;
-    height: 99px;
+    width: ${toRem(350)};
+    height: ${toRem(99)};
     }
     }
 `;
