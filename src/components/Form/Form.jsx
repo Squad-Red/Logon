e08343150b1cb4eb32af007c0926b31e5829e34b
Brunnalage <brunnalage@gmail.com>
2022-06-27 23:08:45 -0300

    Fix: Validation error message bug fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,8 +3,9 @@ import { Input } from '../Input/Input';
 import Button from '../Button/Button';
 import { FormStyled } from './Form.styled';
 import { InputsStyled } from './Form.styled';
-import { FormBtnStyled } from './Form.styled';
+import { FormFooterStyled } from './Form.styled';
 import { useForm } from "react-hook-form";
+import { ErrorMessagesStyled } from './Form.styled';
 
 
 const Form = () => {
@@ -15,13 +16,14 @@ const Form = () => {
         <FormStyled onSubmit={handleSubmit(onSubmit)}>
             <h5>Login</h5>
             <InputsStyled>
-                <Input placeholder="Usuário" type="text" {...{register: register('user',{pattern: /^[\w.-]+\.[\w.-]+$/ })}} />
-                <Input placeholder="Senha" type="password" {...{register: register('password',{pattern: /^(?=.*[0-9])(?!.*[ !@#%^&*_=+-])(?!.*[a-zA-z]).{6,9}$/ })}}/>                                      
+                <Input placeholder="Usuário" type="text" {...{register: register('user',{pattern: /^[\w.-]+\.[\w.-]+$/, required:true })}} />
+                <Input placeholder="Senha" type="password" {...{register: register('password',{pattern: /^(?=.*[0-9])(?!.*[ !@#%^&*_=+-])(?!.*[a-zA-z]).{6,9}$/, required:true })}} required/>                                      
             </InputsStyled>
-            <FormBtnStyled>
-            {errors.user || errors.password && <span>Ops, usuário ou senha inválidos. Tente novamente!</span>}       
+            <FormFooterStyled>
+            {errors.user && <ErrorMessagesStyled>Ops, usuário ou senha inválidos. Tente novamente!</ErrorMessagesStyled>}
+            {errors.password && <ErrorMessagesStyled>Ops, usuário ou senha inválidos. Tente novamente!</ErrorMessagesStyled>}       
                 <Button title="Continuar" type="Submit" id="Login" />
-            </FormBtnStyled>
+            </FormFooterStyled>
         </FormStyled>
     )
 }
