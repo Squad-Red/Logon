0c857b553e3d492a71a9ee502d88d49147e9d85e
Jean-Stefan <jeanss12@gmail.com>
2022-06-29 17:20:13 -0300

    Feat: Added TimerProvider on index.jsx to test

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {GlobalStyle} from '../globalStyles';
 import App from './App';
+import TimerProvider from './contexts/TimerProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <>
         <GlobalStyle />
-        <App />
-    </React.StrictMode>,
+        <TimerProvider>
+            <App />
+        </TimerProvider>
+    </>,
 );
