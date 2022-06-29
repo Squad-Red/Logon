18eb60dface76c3577d6ae666f8c300a86550da3
Jean-Stefan <jeanss12@gmail.com>
2022-06-28 22:01:48 -0300

    Feat: Added || on isError conditional

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,31 +1,69 @@
 import React from 'react';
-import { Input } from '../Input/Input';
+import {Input} from '../Input/Input';
 import Button from '../Button/Button';
-import { FormStyled, InputsStyled, FormButtonStyled, ErrorMessagesStyled, FormErrorStyled, FormTitleStyled } from './Form.styled';
-import { useForm } from "react-hook-form";
+import {
+    FormStyled,
+    InputsStyled,
+    ErrorMessagesStyled,
+    FormErrorStyled,
+    FormTitleStyled,
+} from './Form.styled';
+import {useForm} from 'react-hook-form';
 import Title from '../Title/Title';
 
 const Form = () => {
-    const { register, handleSubmit, formState:{errors} } = useForm();
-    const onSubmit = data => console.log(data);    
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm();
+    const onSubmit = (data) => console.log(data);
 
-        return (  
+    return (
         <FormStyled onSubmit={handleSubmit(onSubmit)}>
             <FormTitleStyled>
-            <Title tagName="h4" text="Login" />
+                <Title tagName='h4' text='Login' />
             </FormTitleStyled>
             <InputsStyled>
-                <Input placeholder="Usuário" type="text" isError={!!errors.user} {...{register: register('user',{pattern: /^[\w.-]+\.[\w.-]+$/, required:true })}} />
-                <Input placeholder="Senha" type="password" isError={!!errors.password}{...{register: register('password',{pattern: /^(?=.*[0-9])(?!.*[ !@#%^&*_=+-])(?!.*[a-zA-z]).{6,9}$/, required:true })}} required/>                                      
+                <Input
+                    placeholder='Usuário'
+                    type='text'
+                    isError={errors.user || errors.password}
+                    {...{
+                        register: register('user', {
+                            pattern: /^[\w.-]+\.[\w.-]+$/,
+                            required: true,
+                        }),
+                    }}
+                />
+                <Input
+                    placeholder='Senha'
+                    type='password'
+                    isError={errors.user || errors.password}
+                    {...{
+                        register: register('password', {
+                            pattern:
+                                /^(?=.*[0-9])(?!.*[ !@#%^&*_=+-])(?!.*[a-zA-z]).{6,9}$/,
+                            required: true,
+                        }),
+                    }}
+                    required
+                />
             </InputsStyled>
             <FormErrorStyled>
-            {(errors.user && <ErrorMessagesStyled>Ops, usuário ou senha inválidos. Tente novamente!</ErrorMessagesStyled>) || 
-            (errors.password && <ErrorMessagesStyled>Ops, usuário ou senha inválidos. Tente novamente!</ErrorMessagesStyled>)}       
+                {(errors.user && (
+                    <ErrorMessagesStyled>
+                        Ops, usuário ou senha inválidos. Tente novamente!
+                    </ErrorMessagesStyled>
+                )) ||
+                    (errors.password && (
+                        <ErrorMessagesStyled>
+                            Ops, usuário ou senha inválidos. Tente novamente!
+                        </ErrorMessagesStyled>
+                    ))}
             </FormErrorStyled>
-            <FormButtonStyled>
-                <Button title="Continuar" type="Submit" id="Login" />
-            </FormButtonStyled>
+            <Button title='Continuar' type='Submit' id='Login' />
         </FormStyled>
-    )
-}
+    );
+};
 export default Form;
