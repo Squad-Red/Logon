2c54252a62acff2ced324db6a8c578c73953b7ef
Jean-Stefan <jeanss12@gmail.com>
2022-06-29 17:20:41 -0300

    Feat: Added structure to test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react';
+import {Timer} from './components/Timer/Timer';
+import Button from './components/Button/Button';
 
-function App() {  
+import {TimerContext} from './contexts/TimerProvider';
 
-  return (
-   <>
-   </>
-  )
+function App() {
+    const [timer, setTimer] = useContext(TimerContext);
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '20px',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '100%',
+                background:
+                    ' linear-gradient(180deg, #33383D 0%, #1C1D20 100%)',
+            }}
+        >
+            <Timer />
+            <Button
+                id={'Login'}
+                title={'Reset'}
+                onClick={() => setTimer(600)}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
