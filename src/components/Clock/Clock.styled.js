679093c7df3b150123e8a316f94795421517b3c2
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-06-29 00:45:38 -0300

    Style: changing the clock component size

--- src/components/Clock/Clock.styled.js
+++ src/components/Clock/Clock.styled.js
@@ -6,15 +6,16 @@ export const ClockDiv = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    margin-top: ${toRem(30)};
 `;
 
-export const HoursStyled = styled.h1` 
+export const HoursStyled = styled.p` 
     font-family: 'Poppins', sans-serif;
     color: #222222;
     font-weight: 700;
-    font-size: ${toRem(144)};
+    font-size: ${toRem(110)};
     margin: 0;    
-    line-height: ${toRem(130)};
+    line-height: ${toRem(100)};
 `;
 
 
@@ -22,6 +23,6 @@ export const DateStyled = styled.p`
     font-family: 'Poppins', sans-serif;
     color: #222222;
     font-weight: 400;
-    font-size: ${toRem(14)};
+    font-size: ${toRem(12)};
     margin: 0;  
 `;
