8a2569e499f981e706b099de6ee06d46c7cea13b
Jean-Stefan <jeanss12@gmail.com>
2022-06-30 15:42:26 -0300

    Feat: Added new divs for buttons

--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -9,12 +9,20 @@ export const FooterStyled = styled.div`
     align-self: flex-end;
     align-items: center;
     background: linear-gradient(90.16deg, #33383d 0%, #1c1d20 100%);
+
+    @media (max-width: 768px) {
+        grid-template-columns: 1fr;
+    }
 `;
 
 export const InfoDiv = styled.div`
     grid-column: 1/2;
     justify-self: flex-end;
     margin-left: ${toRem(40)};
+
+    @media (max-width: 768px) {
+        display: none;
+    }
 `;
 
 export const InteractiveDiv = styled.div`
@@ -22,17 +30,36 @@ export const InteractiveDiv = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    @media (max-width: 768px) {
+        grid-column: 1/-1;
+        width: 100%;
+    }
 `;
 
 export const TimerDiv = styled.div`
     display: flex;
     flex-basis: ${toRem(360)};
     justify-content: flex-end;
+
+    @media (max-width: 768px) {
+        display: none;
+    }
 `;
 
 export const ButtonContainer = styled.div`
     display: flex;
     align-items: center;
+
+    @media (max-width: 768px) {
+        flex-direction: row-reverse;
+        width: 100%;
+    }
 `;
 
-export const ButtonDiv = styled.div``;
+export const LogoutDiv = styled.div``;
+
+export const ResetDiv = styled.div`
+    @media (max-width: 768px) {
+        width: 100%;
+    }
+`;
