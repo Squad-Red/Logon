9241ff66e6e9cbd1b6cbec3c692fc19c8748203e
Jean-Stefan <jeanss12@gmail.com>
2022-06-30 17:45:25 -0300

    Feat: Added router and link to login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
