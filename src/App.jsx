633227514d8bddfc442a811b472b59a9802b1924
Jean-Stefan <jeanss12@gmail.com>
2022-06-30 13:53:36 -0300

    Style: Changed App and Index to the default

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,7 @@
-import React, {useContext, useEffect, useState} from 'react';
-import styled from 'styled-components';
-import {Timer} from './components/Timer/Timer';
-import Button from './components/Button/Button';
-
-import {TimerContext} from './contexts/TimerProvider';
+import React from 'react';
 
 function App() {
-    const [timer, setTimer] = useContext(TimerContext);
-
-    return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '20px',
-                justifyContent: 'center',
-                alignItems: 'center',
-                minHeight: '100%',
-                background:
-                    ' linear-gradient(180deg, #33383D 0%, #1C1D20 100%)',
-            }}
-        >
-            <Timer />
-            <ButtonWrapper>
-                <Button
-                    id={'Login'}
-                    title={'Reset'}
-                    onClick={() => setTimer(600)}
-                />
-            </ButtonWrapper>
-        </div>
-    );
+    return <></>;
 }
 
 export default App;
-
-const ButtonWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-`;
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {GlobalStyle} from '../globalStyles';
 import App from './App';
-import TimerProvider from './contexts/TimerProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         <GlobalStyle />
-        <TimerProvider>
-            <App />
-        </TimerProvider>
+        <App />
     </>,
 );
