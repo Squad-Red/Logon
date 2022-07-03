224b18ee7ca437f73d2d6cfcdaa6c874163715b9
Jean-Stefan <jeanss12@gmail.com>
2022-07-03 18:16:26 -0300

    Feat: Changed API key and hide it on .env

--- src/api/weather.js
+++ src/api/weather.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const weather = axios.create({
     baseURL: 'http://api.weatherapi.com/v1',
     params: {
-        key: '3e80f85332424d23a5e131540222406',
+        key: import.meta.env.VITE_API_KEY,
         lang: 'pt',
     },
 });
