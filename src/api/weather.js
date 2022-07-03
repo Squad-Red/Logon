883979032ca7149392a96113667dacf1652e1ff4
Jean-Stefan <jeanss12@gmail.com>
2022-07-03 18:44:44 -0300

    Fix: Changed request to https

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const weather = axios.create({
-    baseURL: 'http://api.weatherapi.com/v1',
+    baseURL: 'https://api.weatherapi.com/v1',
     params: {
         key: import.meta.env.VITE_API_KEY,
         lang: 'pt',
